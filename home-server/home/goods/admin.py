e7714364b8e633241d52529c29960a0f3d01b58a
Radmir <galiullinradmir17@gmaul.com>
2024-04-08 15:26:00 +0300

    work with admin panel

--- home-server/home/goods/admin.py
+++ home-server/home/goods/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
+from goods.models import Categories
+
+
+admin.site.register(Categories)
--- home-server/home/goods/models.py
+++ home-server/home/goods/models.py
@@ -4,3 +4,8 @@ class Categories(models.Model):
 
     name = models.CharField(max_length=150, unique=True)
     slug = models.SlugField(max_length=200, unique=True, blank=True, null=True)
+
+    class Meta:
+        db_table = 'product_category'
+        verbose_name = 'category'
+        verbose_name_plural = 'categories'
