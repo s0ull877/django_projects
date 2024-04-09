f9f4960e3a2a23390e4c087b3aa96ac41d834a81
Radmir <galiullinradmir17@gmaul.com>
2024-04-09 19:13:42 +0300

    create product model

--- home-server/home/goods/admin.py
+++ home-server/home/goods/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
-from goods.models import Categories
+from goods.models import Categories, Products
 
 
 admin.site.register(Categories)
+
+admin.site.register(Products)
--- home-server/home/goods/models.py
+++ home-server/home/goods/models.py
@@ -1,3 +1,4 @@
+from unicodedata import category
 from django.db import models
 
 class Categories(models.Model):
@@ -6,6 +7,26 @@ class Categories(models.Model):
     slug = models.SlugField(max_length=200, unique=True, blank=True, null=True)
 
     class Meta:
-        db_table = 'product_category'
+        db_table = 'category'
         verbose_name = 'category'
         verbose_name_plural = 'categories'
+
+    def __str__(self) -> str:
+
+        return f'Категория : {self.name}'
+
+
+class Products(models.Model):
+
+    name = models.CharField(max_length=150, unique=True)
+    descriptiom = models.TextField(blank=True,null=True)
+    image = models.ImageField(upload_to='goods_image', blank=True, null=True)
+    price = models.DecimalField(default=0.00, max_digits=7, decimal_places=2)
+    discount = models.DecimalField(default=0.00, max_digits=7, decimal_places=2)
+    quantity = models.PositiveIntegerField(default=0)
+    category = models.ManyToManyField(to=Categories, default=4)
+
+    class Meta:
+        db_table = 'product'
+        verbose_name = 'product'
+        verbose_name_plural = 'products'
