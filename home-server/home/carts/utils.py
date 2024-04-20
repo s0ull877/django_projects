958103c4b51e31f19dae9e186fef2e68093f09c4
Radmir <galiullinradmir17@gmaul.com>
2024-04-20 15:10:49 +0300

    previous commit bug fixed

--- home-server/home/carts/utils.py
+++ home-server/home/carts/utils.py
@@ -3,4 +3,4 @@ from carts.models import HomeCart
 def get_user_carts(request):
 
     if request.user.is_authenticated:
-        return HomeCart.objects.filter(user=request.user)
+        return HomeCart.objects.filter(user=request.user).order_by('created')
