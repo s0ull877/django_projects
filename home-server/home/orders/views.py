3a964ed01bccb96f0b7be372c7be0d562383a19e
Radmir <galiullinradmir17@gmaul.com>
2024-04-25 10:09:35 +0300

    Create_order is ready

--- home-server/home/orders/views.py
+++ home-server/home/orders/views.py
@@ -1,24 +1,93 @@
+from django.db import transaction
+from django.forms import ValidationError
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
+
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
+
 from django.urls import reverse
+
 from carts.utils import get_user_carts
+
+from orders.forms import CreateOrderForm
+from orders.models import Order, OrderItem
 
 
 @login_required
 def create_order(request):
     
     user_carts = get_user_carts(request)
+    user = request.user
 
-    if not user_carts.exists():
-        
-        messages.warning(request, f"Вы не можете оформить заказ, пока корзина пуста!")
+    if request.method == 'POST':
 
-        return HttpResponseRedirect(reverse('goods:catalog', kwargs={'slug':'all'}))
-    
+        form = CreateOrderForm(data=request.POST)
+
+        if form.is_valid():
+            try:
+                with transaction.atomic():
+                    
+                    if user_carts.exists():
+                        
+                        order = Order.objects.create(
+                            user=user,
+                            phone_number=form.cleaned_data['phone_number'],
+                            requires_delivery=form.cleaned_data['requires_delivery'],
+                            delivery_address=form.cleaned_data['delivery_address'],
+                            payment_on_get=form.cleaned_data['payment_on_get'], 
+                        )
+
+                        for user_cart in user_carts:
+                            product = user_cart.product
+                            name = user_cart.product.name
+                            price = user_cart.price
+                            quantity = user_cart.quantity
+
+                            if product.quantity < quantity:
+                                raise ValidationError(f"Insufficient quantity of '{name}' in stock. \
+                                                      In stock - {product.quantity}")
+                            
+                            OrderItem.objects.create(
+                                order=order,
+                                product=product,
+                                name=name,
+                                price=price,
+                                quantity=quantity,
+                            )
+
+                            product.quantity -= quantity
+                            product.save()
+
+
+                        user_carts.delete()
+
+                        messages.success(request, 'Заказ оформлен!')
+                        return HttpResponseRedirect(reverse('users:profile', kwargs={'pk':user.pk}))
+
+            except ValidationError as ex:
+
+                messages.warning(request, str(ex))
+                return HttpResponseRedirect(reverse('order:create'))
+            
     else:
 
-        context = {'title': 'Home - Новый заказ'}
+        if not user_carts.exists():
+            
+            messages.warning(request, f"Вы не можете оформить заказ, пока корзина пуста!")
 
-        return render(request, 'orders/create_order.html', context)
+            return HttpResponseRedirect(reverse('goods:catalog', kwargs={'slug':'all'}))
+        
+        else:
+
+            init = {
+                'first_name': user.first_name,
+                'last_name': user.last_name
+            }
+
+            form = CreateOrderForm(initial=init)
+
+            context = {'title': 'Home - Новый заказ', 'form': form}
+
+            return render(request, 'orders/create_order.html', context)
+        
